9022ad204d32ce8e933c6a8cdf738f04b1c3bb00
vineethpejathaya <vineethpejathaya@gmail.com>
2021-08-11 06:04:57 +0530

    commit code changes for mandatory field

--- Script.js
+++ Script.js
@@ -9,57 +9,65 @@ let month = document.querySelector('.month');
 let day = document.querySelector('.day');
 
 btn_calc.addEventListener("click",function(e) {
-            
-             let currentDate= new Date();
-             let inputDateValue = new Date(inputDate.value);
-             
-             
-
-             birthYear = inputDateValue.getFullYear();
-             birthMonth= inputDateValue.getMonth()+1;
-             birthDay= inputDateValue.getDate();
-
-             currentYear = currentDate.getFullYear();
-             currentMonth = currentDate.getMonth()+1;
-             currentDay = currentDate.getDate();
-             console.log(currentDay);
-
-             leapYear(currentYear)  ;   
-
-             if ( birthYear > currentYear || currentYear == birthYear && birthMonth > currentMonth 
-                || currentYear == birthYear && birthMonth == currentMonth && birthDay > currentDay)
-            
+            if(inputDate.value !='')
             {
+                    document.querySelector('small').className = "small";
+                    
+                    let currentDate= new Date();
+                    let inputDateValue = new Date(inputDate.value);
+                    
+                    
 
-                alert("Not Born yet");
-            }
-            
-            
-            yearOld = currentYear - birthYear;
+                    birthYear = inputDateValue.getFullYear();
+                    birthMonth= inputDateValue.getMonth()+1;
+                    birthDay= inputDateValue.getDate();
 
-            if( currentMonth >= birthMonth)
-            {
-                monthOld = currentMonth - birthMonth;
+                    currentYear = currentDate.getFullYear();
+                    currentMonth = currentDate.getMonth()+1;
+                    currentDay = currentDate.getDate();
+                    console.log(currentDay);
+
+                    leapYear(currentYear)  ;   
+
+                    if ( birthYear > currentYear || currentYear == birthYear && birthMonth > currentMonth 
+                        || currentYear == birthYear && birthMonth == currentMonth && birthDay > currentDay)
+                    
+                    {
+
+                        alert("Not Born yet");
+                    }
+                    
+                    
+                    yearOld = currentYear - birthYear;
+
+                    if( currentMonth >= birthMonth)
+                    {
+                        monthOld = currentMonth - birthMonth;
+                    }
+                    else
+                    {
+                        yearOld--;
+                        monthOld = 12 - (birthMonth - currentMonth) ; 
+                    }
+
+                    if(currentDay >= birthDay)
+                    {
+                        dayOld = currentDay - birthDay;
+                    }
+                    else
+                    {
+                        monthOld--;
+                        dayOld = (months[currentMonth-1] - birthDay ) + currentDay ;
+                    }
+                    
+                    year.innerHTML = yearOld;
+                    month.innerHTML = monthOld;
+                    day.innerHTML = dayOld;
             }
             else
             {
-                yearOld--;
-                monthOld = 12 - (birthMonth - currentMonth) ; 
+                    document.querySelector('small').className = "smart";
             }
-
-            if(currentDay >= birthDay)
-            {
-                dayOld = currentDay - birthDay;
-            }
-            else
-            {
-                monthOld--;
-                dayOld = (months[currentMonth-1] - birthDay ) + currentDay ;
-            }
-            
-            year.innerHTML = yearOld;
-            month.innerHTML = monthOld;
-            day.innerHTML = dayOld;
 })
 
 
